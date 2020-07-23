169da8db7d3d6b8a692ae2a77d6cc31ad4771ee7
Ankit Saxena <Ankit.Saxena@mail.weir>
2020-07-23 20:14:12 +0530

    Added documentation & in-script comments

--- dlib_blink_detection.py
+++ dlib_blink_detection.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+# =====================================================================
+#  Filename:    dlib_blink_detection.py
+#
+#  py Ver:      python 3.6 or later
+#
+#  Description: Detects how many times a person blinks on a video-stream.
+#
+#  Usage: python dlib_blink_detection.py --shape_predictor shape_predictor_68_face_landmarks.dat
+#         or
+#         python dlib_blink_detection.py --video test.mp4 --shape_predictor shape_predictor_68_face_landmarks.dat
+#
+#  Note: Requires opencv 3.4.2 or later
+#
+#  Author: Ankit Saxena (ankch24@gmail.com)
+# =====================================================================
+
 import argparse
 import cv2
 import dlib
@@ -9,7 +26,12 @@ from scipy.spatial import distance
 
 
 def eye_aspect_ratio(eye):
-
+    """
+    Calculates the ratio of the vertical to the horizontal width of the eye detected. A dip in the ratio corresponds
+    to a blink
+    :param eye: numpy array with eye landmark co-ordinates
+    :return: eye aspect ratio
+    """
     vertical_1 = distance.euclidean(eye[1], eye[5])
     vertical_2 = distance.euclidean(eye[2], eye[4])
     horizontal = distance.euclidean(eye[0], eye[3])
@@ -32,10 +54,13 @@ def get_arguments():
 
 def main(video, predictor, ratio_thresh, frame_thresh):
 
+    # loading the face detector
     print('[INFO] Loading facial landmark predictor...')
     detector = dlib.get_frontal_face_detector()
+    # loading the facial landmark predictor
     predictor = dlib.shape_predictor(predictor)
 
+    # saving the landmark indices for the left & the right eye
     left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
     right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
@@ -66,19 +91,27 @@ def main(video, predictor, ratio_thresh, frame_thresh):
         frame = imutils.resize(frame, width=450)
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # detect all faces in the frame
         rectangles = detector(gray_frame, 0)
 
+        # iterate over al faces
         for face in rectangles:
+
+            # locate all facial landmarks in the frame
             shape = face_utils.shape_to_np(predictor(gray_frame, face))
 
+            # isolate the left & the right eye
             left_eye = shape[left_eye_start:left_eye_end]
             right_eye = shape[right_eye_start:right_eye_end]
 
+            # find the aspect ratio for both the eyes
             left_ratio = eye_aspect_ratio(left_eye)
             right_ratio = eye_aspect_ratio(right_eye)
 
+            # average out the ratio
             avg_ratio = (left_ratio + right_ratio) / 2
 
+            # draw contours around the eyes
             left_eye_curve = cv2.convexHull(left_eye)
             right_eye_curve = cv2.convexHull(right_eye)
 
@@ -86,14 +119,18 @@ def main(video, predictor, ratio_thresh, frame_thresh):
             cv2.drawContours(frame, [right_eye_curve], -1, (0, 255, 0), 1)
 
             if avg_ratio < ratio_thresh:
+                # increase number of consecutive frames for which the eyes have been closed
                 counter += 1
 
             else:
                 if counter >= frame_thresh:
+                    # increase number of blinks if the eyes have been closed for more than the threshold number of
+                    # frames
                     blinks += 1
 
                 counter = 0
 
+            # label the frame
             cv2.putText(frame, f'Blinks: {blinks}', (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             cv2.putText(frame, f'iRatio: {round(avg_ratio, 2)}', (300, 30),
