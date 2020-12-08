b54b8c5735f65651cba6ddea50bffbfbeb624410
Ankit-Saxena-weir <60417729+Ankit-Saxena-weir@users.noreply.github.com>
2020-12-08 23:32:05 +0530

    Refined the flask deployment

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def gen(feed):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route("/test_link")
+@app.route("/explain/")
 def test_link():
     return render_template('how_it_works.html')
 
 
-@app.route('/video_feed')
+@app.route('/video_feed/')
 def video_feed():
     global video
     return Response(web_main(video, 'shape_predictor_68_face_landmarks.dat', ratio_thresh=0.3, frame_thresh=2),
